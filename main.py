e97b18c9ce1c7c3418e75fe4a0d1d1fdb9abe9fb
Burakblm <93054880+Burakblm@users.noreply.github.com>
2022-03-10 16:44:08 +0300

    preprocess fonksiyonu tanımlandı

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ image_path = ''
 
 
 def preprocess(image):
-
-    pass
+    # Resmi BGR renk tonlamasından siyah beyaza çeviriyoruz
+    image = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+    
+    # Resmi sınıflandırma algoritmamızın girdi boyutu olan (28x28) boyutlarına getiriyoruz
+    image = cv.resize(image,(28,28))
+    
+    # Sınıflandırma algoritmamız girdi olarak bir vertör aldığı için resmi vektör haline getiriyoruz
+    image = image.reshape(784)
+    
+    return image
