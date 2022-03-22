6a3e8bb34ae4cf40d2b697155285442fb6db968f
Burakblm <93054880+Burakblm@users.noreply.github.com>
2022-03-22 18:28:06 +0300

    cnn2D fonksiyonu tamamlandı

--- Training.py
+++ Training.py
@@ -36,12 +36,60 @@ def convert_1d_to_2d():
 
 
 def cnn2d():
-    """
+    train_X = []
+    test_X = []
+    
+    for i in range(emnist_train_x.shape[0]):
+        a = np.resize(x_train[i],(28,28))
+        a = np.transpose(a)
+        train_X.append(a)
 
-    :return:
-    """
-    pass
+    for i in range(emnist_test_x.shape[0]):
+        a = np.resize(x_test[i],(28,28))
+        a = np.transpose(a)
+        test_X.append(a)
+        
+        
+    x_train = np.array(train_X)
+    x_test = np.array(test_X)
 
+    x_train = x_train.reshape((x_train.shape[0],28,28,1))
+    x_test = x_test.reshape((x_test.shape[0],28,28,1))
+    
+    input_shape = (28,28,1)
+    
+    model = keras.Sequential(
+        [
+            keras.Input(shape=input_shape),
+            layers.Conv2D(64,kernel_size=(3,3),padding="same" ,activation="relu"),
+            layers.MaxPooling2D(pool_size=(2,2)),
+            layers.BatchNormalization(),
+            layers.Dropout(0.3),
+            layers.Conv2D(128,kernel_size=(3,3),padding="same" ,activation="relu"),
+            layers.MaxPooling2D(pool_size=(2,2)),
+            layers.BatchNormalization(),
+            layers.Dropout(0.3),
+            layers.Conv2D(256,kernel_size=(3,3),padding="same", activation="relu"),
+            layers.MaxPooling2D(pool_size=(2,2)),
+            layers.BatchNormalization(),
+            layers.Flatten(),
+            layers.Dense(128,activation="relu"),
+            layers.Dense(64,activation="relu"),
+            layers.Dropout(0.3),
+            layers.Dense(47,activation="softmax"),
+        ]
+    )
+    model.summary()
+    
+    batch_size = 128
+    epochs = 10
+
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+    
+    open("harf_siniflandirma_emnist_model.json","w").write(model.to_json())# modeli kaydetmek için kullanılır
+    model.save_weights("harf_sinif_emnist.h5")
 
 def cnn1d_2layer():
     """
